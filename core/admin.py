94cd3c9b7abbb2f3bccc6c18dbb793574a3b547d
Rodrigo <rousuy@gmail.com>
2021-04-07 16:22:06 -0300

    Criado páginas de testes em html utilizando Bootstrap4 para exemplicar

---
+++ core/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Cargo, Servico, Funcionario, Recurso
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'modificado', 'ativo')
+
+
+@admin.register(Recurso)
+class RecursoAdmin(admin.ModelAdmin):
+    lista_display = ('recurso', 'icone', 'ativo', 'modificado')
